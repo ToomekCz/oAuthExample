11588edb6f4b906b373c4b2526a388b2ecccd05a
tomek1525 <tomek15252@wp.pl>
2017-06-11 23:58:11 +0200

    Initial

---
+++ oAuthTest/CustomAuthorizationServerProvider.cs
@@ -0,0 +1,54 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OAuth;
+
+namespace oAuthTest
+{
+    internal class CustomAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            //string clientId;
+            //string clientSecret;
+            //context.TryGetFormCredentials(out clientId, out clientSecret);
+
+            //if (clientId == "1234" && clientSecret == "12345")
+            //{
+            //    context.Validated(clientId);
+            //}
+
+            //return base.ValidateClientAuthentication(context);
+            context.Validated();
+            return base.ValidateClientAuthentication(context);
+        }
+
+        public override Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            //Method name : GrantClientCredentials
+
+            //var oAuthIdentity = new ClaimsIdentity(context.Options.AuthenticationType);
+            //oAuthIdentity.AddClaim(new Claim(ClaimTypes.Name, "SuperClient"));
+            //var ticket = new AuthenticationTicket(oAuthIdentity, new AuthenticationProperties());
+            //context.Validated(ticket);
+            //return base.GrantClientCredentials(context);
+
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            var validated = context.UserName != "userName" || context.Password != "password";
+
+            if (validated)
+            {
+                context.SetError("invalid_grant", "The user name or password is incorrect.");
+                return base.GrantResourceOwnerCredentials(context);
+            }
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+
+            context.Validated(identity);
+            return base.GrantResourceOwnerCredentials(context);
+        }
+    }
+}
---
+++ oAuthTest/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+
+[assembly: OwinStartup(typeof(oAuthTest.Startup))]
+
+namespace oAuthTest
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new {id = RouteParameter.Optional}
+            );
+
+            ConfigureOAuth(app);
+
+            app.UseWebApi(config);
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+
+        private void ConfigureOAuth(IAppBuilder app)
+        {
+           OAuthAuthorizationServerOptions oAuthServerOptions = new OAuthAuthorizationServerOptions()
+           {
+               AllowInsecureHttp = true,
+               TokenEndpointPath = new PathString("/token"),
+               AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+               Provider = new CustomAuthorizationServerProvider()
+           };
+
+            app.UseOAuthAuthorizationServer(oAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
